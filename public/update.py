84e6e7fa13b29ea77006db586d68c80bc37ff67c
maddoxx099 <ishmitsingh462.is@gmail.com>
2024-08-10 11:48:43 +0530

    query

--- public/update.py
+++ public/update.py
@@ -1,5 +1,5 @@
 array = [["ashaf", "jukusuke" ,"ryuuichi","asuka","noroi" ,"kenji" ,"asura" ,"tashiro" ,"tadashi","seina" ,"kaze","ishika" ,"akane","sado" ,"alaric" , "gorou","kaiaku",  "arata","chiyomi","kuba","touka","abo", "kazuya","hanako","higeki","isamu","enki"],
-         [42, 41, 32, 32, 31, 30,25,25,24, 24,23,23,23,21,19,18,17,15,14,20,20,20,20,21,20,20,20]]
+         [42, 41, 32, 33.28, 31, 32.1,25,25,24, 24,23,23,23,21,19,18,17,15,14,20,20,20,20,21,20,20,20]]
 output = ""
 for i,value in enumerate(array[0]):
     output+= str(value)+"*"+ str(array[1][i])+"+"
