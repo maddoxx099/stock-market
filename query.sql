9a93742bc1f3b2bdd4499d1325c8097db5388942
maddoxx099 <ishmitsingh462.is@gmail.com>
2024-08-13 12:48:16 +0530

    new entries

--- query.sql
+++ query.sql
@@ -1,2 +1,2 @@
 INSERT INTO stockvalues (ashaf, jukusuke, ryuuichi, asuka, noroi, kenji, asura, tashiro, tadashi, seina, kaze, ishika, akane, sado, alaric, gorou, kaiaku, arata, chiyomi, kuba, touka, abo, kazuya, hanako,higeki,isamu,enki,yuu,ue,shiro,shukei,hokama,lastchapter)
-VALUES (42, 41, 32, 33.28, 31, 32.1,25,25,24, 24,23,23,23,21,19,18,17,15,14,20,20,20,20,21,20,20,20,20,20,20,20,20,2);
+VALUES (42, 44.69, 35.84, 33.28, 31, 32.1,27.25,25.5,24, 24,23,23.46,23,21,19,18,17,15.15,14,20,20,20,20,21,20,20,20,20.2,20,20,20,20,2);
--- query2.sql
+++ query2.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 UPDATE users set tempProfit=totalstockvalue;
-UPDATE users set totalstockvalue=ashaf*42+jukusuke*41+ryuuichi*32+asuka*33.28+noroi*31+kenji*32.1+asura*25+tashiro*25+tadashi*24+seina*24+kaze*23+ishika*23+akane*23+sado*21+alaric*19+gorou*18+kaiaku*17+arata*15+chiyomi*14+kuba*20+touka*20+abo*20+kazuya*20+hanako*21+higeki*20+isamu*20+enki*20+yuu*20+ue*20+shiro*20+shukei*20+hokama*20+money;
+UPDATE users set totalstockvalue=ashaf*42+jukusuke*44.69+ryuuichi*35.84+asuka*33.28+noroi*31+kenji*32.1+asura*27.25+tashiro*25.5+tadashi*24+seina*24+kaze*23+ishika*23.46+akane*23+sado*21+alaric*19+gorou*18+kaiaku*17+arata*15.15+chiyomi*14+kuba*20+touka*20+abo*20+kazuya*20+hanako*21+higeki*20+isamu*20+enki*20+yuu*20.2+ue*20+shiro*20+shukei*20+hokama*20+money;
 UPDATE users set tempProfit=totalstockvalue-tempProfit;
 UPDATE users set profit=profit+tempProfit;
 
