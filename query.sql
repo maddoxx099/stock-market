cfcf4c4207692d89be22441611a762b9ecfd14bb
maddoxx099 <ishmitsingh462.is@gmail.com>
2024-08-16 19:20:22 +0530

    new update

--- query.sql
+++ query.sql
@@ -1,2 +1,2 @@
 INSERT INTO stockvalues (ashaf, jukusuke, ryuuichi, asuka, noroi, kenji, asura, tashiro, tadashi, seina, kaze, ishika, akane, sado, alaric, gorou, kaiaku, arata, chiyomi, kuba, touka, abo, kazuya, hanako,higeki,isamu,enki,yuu,ue,shiro,shukei,hokama,wei,lastchapter)
-VALUES (39.9, 44.69, 35.84, 33.28, 31, 32.1,27.25,25.5,24, 24,23,23.46,23,23.52,19,18,19.04,15.15,14.7,20,21,20,22.4,21,20,20,18,20.2,20,20,20,20,20,5);
+VALUES (39.9, 44.69, 35.84, 33.28, 31, 32.1,27.25,25.5,24, 24,23,23.46,23,25.4,19,18,20.75,15.15,14.7,20,22.05,20,24.2,21,20,20,18,20.2,20,20,20,22.2,20,6);
