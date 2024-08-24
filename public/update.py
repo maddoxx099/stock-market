f86e26f81e91d187959d8b7d2d64c01d31f9bcb3
maddoxx099 <ishmitsingh462.is@gmail.com>
2024-08-24 19:20:42 +0530

    stock report

--- public/update.py
+++ public/update.py
@@ -1,5 +1,5 @@
 array = [["ashaf", "jukusuke" ,"ryuuichi","asuka","noroi" ,"kenji" ,"asura" ,"tashiro" ,"tadashi","seina" ,"kaze","ishika" ,"akane","sado" ,"alaric" , "gorou","kaiaku",  "arata","chiyomi","kuba","touka","abo", "kazuya","hanako","higeki","isamu","enki","yuu","ue","shiro","shukei","hokama","wei","sanagi","meatloaf","amelie","shihai","yasu"],
-         [39 , 44.69 , 35.84 , 33.28 , 31.62 , 32.1,27.25,27.29,24, 25.44,23.69,25.8,23,25.4,19,18.54,20.75,16.1,14.7,21.6,23.81,20,24.2,21,20,20,17.64,21.41,20,20.6,20,22.2,20,20,2.13,20,10,20]]
+         [39 , 44.69 , 35.84 , 33.28 , 31.62 , 32.1,27.25,27.29,24, 29.26,23.69,25.8,23,25.4,19.57,18.54,20.75,16.1,14.7,21.6,23.81,20,24.2,21,20,20,17.64,21.41,20,20.6,20,22.2,20,20,2.25,20,10,20]]
 output = ""
 for i,value in enumerate(array[0]):
     output+= str(value)+"*"+ str(array[1][i])+"+"
--- query.sql
+++ query.sql
@@ -1,2 +1,2 @@
 INSERT INTO stockvalues (ashaf, jukusuke, ryuuichi, asuka, noroi, kenji, asura, tashiro, tadashi, seina, kaze, ishika, akane, sado, alaric, gorou, kaiaku, arata, chiyomi, kuba, touka, abo, kazuya, hanako,higeki,isamu,enki,yuu,ue,shiro,shukei,hokama,wei,sanagi,meatloaf,amelie,shihai,yasu,lastchapter)
-VALUES (39 , 44.69 , 35.84 , 33.28 , 31.62 , 32.1,27.25,27.29,24, 25.44,23.69,25.8,23,25.4,19,18.54,20.75,16.1,14.7,21.6,23.81,20,24.2,21,20,20,17.64,21.41,20,20.6,20,22.2,20,20,2.13,20,10,20,11);
+VALUES (39 , 44.69 , 35.84 , 33.28 , 31.62 , 32.1,27.25,27.29,24, 29.26,23.69,25.8,23,25.4,19.57,18.54,20.75,16.1,14.7,21.6,23.81,20,24.2,21,20,20,17.64,21.41,20,20.6,20,22.2,20,20,2.25,20,10,20,12);
