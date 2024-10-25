f8ac8f6013677cb8705e4f9117ebc54840927818
maddoxx099 <ishmitsingh462.is@gmail.com>
2024-10-25 21:59:26 +0530

    new

--- git.sh
+++ git.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
 	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
 	&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
