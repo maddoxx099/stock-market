eb282dc797a923b4eca7df590642bf916590bf00
maddoxx099 <ishmitsingh462.is@gmail.com>
2025-05-07 23:46:41 +0530

    m

--- adduser.sql
+++ adduser.sql
@@ -1,11 +1,4 @@
 alter table users
-ADD COLUMN ikiryo integer DEFAULT 0,
-ADD COLUMN shyythiel integer DEFAULT 0,
-ADD COLUMN ain integer DEFAULT 0,
-ADD COLUMN doctor integer DEFAULT 0,
-ADD COLUMN torako integer DEFAULT 0,
-ADD COLUMN eiko integer DEFAULT 0,
-ADD COLUMN hokori_gojo integer DEFAULT 0;
 ADD COLUMN shion integer DEFAULT 0,
 ADD COLUMN kizu integer DEFAULT 0,
 ADD COLUMN darius integer DEFAULT 0,
