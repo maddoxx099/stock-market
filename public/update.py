00a8240c83ef9d91fe216549c8110f17407a0d8b
maddoxx099 <ishmitsingh462.is@gmail.com>
2025-05-08 00:45:59 +0530

    Stock update

--- public/update.py
+++ public/update.py
@@ -1,5 +1,5 @@
 array = [["ashaf", "jukusuke" ,"ryuuichi","asuka","noroi" ,"kenji" ,"asura" ,"tashiro" ,"tadashi","seina" ,"kaze","ishika" ,"akane","sado" ,"alaric" , "gorou","kaiaku",  "arata","chiyomi","kuba","touka","abo", "kazuya","hanako","higeki","isamu","enki","yuu","ue","shiro","shukei","hokama","wei","sanagi","meatloaf","amelie","shihai","yasu","hirioki","renzo","kota","itachi","hikari","sora","kairijo","aki","toru","aoyama","dai","ikiryo","shyythiel","ain","doctor","torako","eiko","hokori_gojo","shion","kizu","darius","yoshinori","ghose","kaien","suzuki"],
-         [39 , 46.03 , 37.63 , 34.61 , 31.62 , 33.7,27.25,27.29,24, 29.26,23.69,23.22,24.15,25.4,19.57,18.54,21.37,16.1,14.7,21.6,23.81,20,24.2,21,21.2,20.6,17.64,21.41,20,21.63,20,22.2,20,20,2,20,10,20,20.6,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,]]
+         [28 , 50 , 43 , 39 , 31.62 , 33.7, 27.25, 27.29, 24, 31, 26, 27, 28, 21, 19.57, 20, 25, 21, 17, 24, 24.5, 1, 24.2, 23.1, 21.2, 22.5, 17.64, 21.41, 20, 20.4, 19, 25.1, 20, 20, 1, 21, 1, 20, 20.6, 22.1, 24.3, 18.6, 20, 20, 20, 22.3, 23.4, 20, 20, 20, 20, 22.6, 20, 17.8, 21.1, 21.4, 22.3, 20, 22.7, 20, 20, 20, 21.8]]
 output = ""
 for i,value in enumerate(array[0]):
     output+= str(value)+"*"+ str(array[1][i])+"+"
--- query2.sql
+++ query2.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 UPDATE users set tempProfit=totalstockvalue;
-UPDATE users set totalstockvalue=ashaf*39+jukusuke*46.03+ryuuichi*37.63+asuka*34.61+noroi*31.62+kenji*33.7+asura*27.25+tashiro*27.29+tadashi*24+seina*29.26+kaze*23.69+ishika*23.22+akane*24.15+sado*25.4+alaric*19.57+gorou*18.54+kaiaku*21.37+arata*16.1+chiyomi*14.7+kuba*21.6+touka*23.81+abo*20+kazuya*24.2+hanako*21+higeki*21.2+isamu*20.6+enki*17.64+yuu*21.41+ue*20+shiro*21.63+shukei*20+hokama*22.2+wei*20+sanagi*20+meatloaf*2+amelie*20+shihai*10+yasu*20+hirioki*20.6+renzo*20+kota*20+itachi*20+hikari*20+sora*20+kairijo*20+aki*20+toru*20+aoyama*20+dai*20+ikiryo*20+shyythiel*20+ain*20+doctor*20+torako*20+eiko*20+hokori_gojo*20+shion*20+kizu*20+darius*20+yoshinori*20+ghose*20+kaien*20+suzuki*20+money;
+UPDATE users set totalstockvalue=ashaf*28+jukusuke*50+ryuuichi*43+asuka*39+noroi*31.62+kenji*33.7+asura*27.25+tashiro*27.29+tadashi*24+seina*31+kaze*26+ishika*27+akane*28+sado*21+alaric*19.57+gorou*20+kaiaku*25+arata*21+chiyomi*17+kuba*24+touka*24.5+abo*1+kazuya*24.2+hanako*23.1+higeki*21.2+isamu*22.5+enki*17.64+yuu*21.41+ue*20+shiro*20.4+shukei*19+hokama*25.1+wei*20+sanagi*20+meatloaf*1+amelie*21+shihai*1+yasu*20+hirioki*20.6+renzo*22.1+kota*24.3+itachi*18.6+hikari*20+sora*20+kairijo*20+aki*22.3+toru*23.4+aoyama*20+dai*20+ikiryo*20+shyythiel*20+ain*22.6+doctor*20+torako*17.8+eiko*21.1+hokori_gojo*21.4+shion*22.3+kizu*20+darius*22.7+yoshinori*20+ghose*20+kaien*20+suzuki*21.8+money;
 UPDATE users set tempProfit=totalstockvalue-tempProfit;
 UPDATE users set profit=profit+tempProfit;
 
