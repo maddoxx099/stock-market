888e89746f78efb771e1347abb17532dfc0ff8c2
maddoxx099 <ishmitsingh462.is@gmail.com>
2025-05-08 14:08:12 +0530

    work

--- public/js/index.js
+++ public/js/index.js
@@ -393,7 +393,7 @@ let lastChapter;
 let sellNum = 1;
 let buyNum = 1;
 canvas.id = "stockGraph";
-const stockMax = 80;
+const stockMax = 100;
 let money = 1000;
 let profit=0;
 let totalstockvalue=1000;
